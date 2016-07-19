e996ba9385ccf9ec9fe6b51adda7429e3b7d46b5
antashik <antashik@yandex.ru>
2016-07-19 14:02:15 +0300

    чертежник

--- drawman.py
+++ drawman.py
@@ -1,6 +1,14 @@
 from turtle import Turtle
-t = Turtle()
-def test_drawmav():
+
+def init_drawman():
+    global t, x_current, y_current
+    t = Turtle()
+    t.penup()
+    x_current = 0
+    y_current = 0
+    t.goto(x_current,y_current)
+
+def test_drawman():
     pen_down()
     for i in range (5):
         on_vector(1,2)
@@ -8,17 +16,24 @@ def test_drawmav():
     pen_up()
     to_point(0,0)
 
-def pen_dowv():
-    pass
+def pen_down():
+    t.pendown()
 
 def pen_up():
-    pass
+    t.penup()
 
-def on_vector():
-    pass
+def on_vector(dx,dy):
+    global x_current, y_current
+    x_current += dx
+    y_current += dy
+    t.goto(x_current+dx,y_current+dy)
 
 def to_point(x,y):
-    pass
+    global x_current, y_current
+    x_current =x
+    y_current =y
+    t.goto(x,y)
 
+init_drawman()
 if __name__=='__main__':
     test_drawman ()
