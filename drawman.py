c27feda216480b5a52677b5d6fb780d48452f411
antashik <antashik@yandex.ru>
2016-07-19 14:26:05 +0300

    чертежник с использованием ГРИС Черепаха

--- drawman.py
+++ drawman.py
@@ -11,8 +11,8 @@ def init_drawman():
 def test_drawman():
     pen_down()
     for i in range (5):
-        on_vector(1,2)
-        on_vector(0,-2)
+        on_vector(10,20)
+        on_vector(0,-20)
     pen_up()
     to_point(0,0)
 
@@ -26,14 +26,16 @@ def on_vector(dx,dy):
     global x_current, y_current
     x_current += dx
     y_current += dy
-    t.goto(x_current+dx,y_current+dy)
+    t.goto(x_current,y_current)
 
 def to_point(x,y):
     global x_current, y_current
     x_current =x
     y_current =y
-    t.goto(x,y)
+    t.goto(x_current,y_current)
 
 init_drawman()
 if __name__=='__main__':
+    import time
     test_drawman ()
+    time.sleep(10)
