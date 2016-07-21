3bd58ee272f1fa7f69178c581af6bebd69917a6f
antashik <antashik@yandex.ru>
2016-07-21 22:17:18 +0300

    Исправлена ошибка масштабирования Чертежника

--- task_dm2.py
+++ task_dm2.py
@@ -1,10 +1,10 @@
 from drawman import *
 from time import sleep
-
 drawman_draw_grid ('blue')
+t.pencolor('black')
 def f(x):
     return x*x
-drawman_scale(100)
+drawman_scale(50)
 x=-5.0
 to_point(x,f(x))
 pen_down()
