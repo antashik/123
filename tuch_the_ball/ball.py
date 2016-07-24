7d3a6b16726e5e8323b32864f4f9ebeab536478e
antashik <antashik@yandex.ru>
2016-07-24 09:03:59 +0300

    Кнопочка эксперименту

--- tuch_the_ball/ball.py
+++ tuch_the_ball/ball.py
@@ -1,13 +1,30 @@
 import tkinter
 
+def button1_command():
+    print ('button 1 default command')
 def print_hellow(event):
-    print('Hellow!')
-    print(dir(event))
+    #print(event.char)
+    #print(event.keycode)
+    print(event.num)
+    print(event.x,event.y)
+    #print(event.x_root, event.y_root)
+    me = event.widget
+    if me == button1:
+        print('Hellow')
+    elif me == button2:
+        print('You press button2!')
+    else :
+        raise ValueError()
+
 
 root = tkinter.Tk()
-button = tkinter.Button (root, text ="Button")
-button.bind("Button-1", print_hellow)
-button.pack()
+button1 = tkinter.Button(root, text="Button 1", command=button1_command)
+button1.bind("<Button>", print_hellow)
+button1.pack()
+
+button2 = tkinter.Button(root, text="Button 2")
+button2.bind("<Button>", print_hellow)
+button2.pack()
 
 root.mainloop()
  
