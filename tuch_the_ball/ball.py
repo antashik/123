b4771dbff3826f2e92ed841bbb72062cde691399
antashik <antashik@yandex.ru>
2016-07-24 10:18:00 +0300

    Кнопочка эксперимент

--- tuch_the_ball/ball.py
+++ tuch_the_ball/ball.py
@@ -16,15 +16,25 @@ def print_hellow(event):
     else :
         raise ValueError()
 
+def init_main_window():
+    global root, button1, button2, label, text, scale
+    root = tkinter.Tk()
 
-root = tkinter.Tk()
-button1 = tkinter.Button(root, text="Button 1", command=button1_command)
-button1.bind("<Button>", print_hellow)
-button1.pack()
+    button1 = tkinter.Button(root, text="Button 1", command=button1_command)
+    button1.bind("<Button>", print_hellow)
 
-button2 = tkinter.Button(root, text="Button 2")
-button2.bind("<Button>", print_hellow)
-button2.pack()
+    button2 = tkinter.Button(root, text="Button 2")
+    button2.bind("<Button>", print_hellow)
 
-root.mainloop()
- 
+    variable = tkinter.IntVar(0)
+    label = tkinter.Label(root, textvariable=variable)
+    scale = tkinter.Scale (root, orient=tkinter.HORIZONTAL, length=300,
+                        from_=0,to=100,tickinterval=10,resolution=5, variable=variable)
+    text = tkinter.Entry(root, textvariable = variable)
+
+    for obj in button1, button2,label, scale, text:
+        obj.pack()
+
+if __name__ == "__main__":
+    init_main_window()
+    root.mainloop()
